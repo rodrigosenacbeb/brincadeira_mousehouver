27e0fff898338f5625eaf0b3e891090aa101d35e
RODRIGO GONCALVES SANTANA <rodrigo.gsantana1@senacsp.edu.br>
2020-01-23 11:57:25 -0300

    atividade pronta

---
+++ Eventos/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace Eventos
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(327, 182);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Clique Aqui";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.MouseHover += new System.EventHandler(this.button1_MouseHover);
+            // 
+            // button2
+            // 
+            this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(547, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(327, 182);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Clique Aqui";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.MouseHover += new System.EventHandler(this.button2_MouseHover);
+            // 
+            // button3
+            // 
+            this.button3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(12, 352);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(327, 182);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Clique Aqui";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.MouseHover += new System.EventHandler(this.button3_MouseHover);
+            // 
+            // button4
+            // 
+            this.button4.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(547, 352);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(327, 182);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Clique Aqui";
+            this.button4.UseVisualStyleBackColor = true;
+
+            this.button4.MouseHover += new System.EventHandler(this.button4_MouseHover);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(886, 546);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ Eventos/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Eventos
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        void TocarAudio()
+        {
+            System.Media.SoundPlayer player = new System.Media.SoundPlayer("errou_fastao.wav");
+            player.Play();
+        }
+
+        private void button1_MouseHover(object sender, EventArgs e)
+        {
+            button1.Visible = false;
+            button2.Visible = true;
+            button3.Visible = true;
+            button4.Visible = true;
+            TocarAudio();
+        }
+
+        private void button2_MouseHover(object sender, EventArgs e)
+        {
+            button1.Visible = true;
+            button2.Visible = false;
+            button3.Visible = true;
+            button4.Visible = true;
+            TocarAudio();
+        }
+
+        private void button3_MouseHover(object sender, EventArgs e)
+        {
+            button1.Visible = true;
+            button2.Visible = true;
+            button3.Visible = false;
+            button4.Visible = true;
+            TocarAudio();
+        }     
+
+        private void button4_MouseHover(object sender, EventArgs e)
+        {
+            button1.Visible = true;
+            button2.Visible = true;
+            button3.Visible = true;
+            button4.Visible = false;
+            TocarAudio();
+        }
+    }
+}
